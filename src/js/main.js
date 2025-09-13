ad8606211cd8b8a2594db9bc07a3fcf021479827
Joaquin Gonzalez <joaquingonzalezjlg@gmail.com>
2025-09-13 14:10:51 -0300

    Connectingbody

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// const accordionButton = document.querySelector(".accordion-item");
+// const accordionBody = document.querySelector(".accordion-body");
+// 
+// accordionButton.addEventListener("click", () => {
+    // accordionBody.classList.toggle("c-active");
+// })
+
